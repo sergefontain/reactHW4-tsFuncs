8b4771c70992e93143fa72baa8b6fdb36f204496
sergefontain <sergefontain@gmail.com>
2020-11-05 18:14:43 +0200

    HW4 component lifecycle on func fixed

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,52 +1,62 @@
 import React, { FC } from "react"
 
-const TimerFC: FC<{}> = ({}) => {
+const TimerFC: FC = () => {
     const [countMin, setCountMin] = React.useState("00")
     const [countSec, setCountSec] = React.useState("00")
     const [countMs, setCountMs] = React.useState("00")
 
     React.useEffect(() => {
-        /**
-         * Did mount section start
-         */
-        let interval = setInterval(() => handleMSeconds(), 10)
-        /**
-         * Did mount section end
-         */
+        console.log("component did mount")
+        const consoleWrite = () => {
+            console.log(
+                "useEffect с пустым массивом зависимостей, где установлено прослушивание клика на всем документе для логирования происходящих событий в консоли"
+            )
+        }
+        const consoleFin = () => console.log("component unmount")
+
+        document.addEventListener("click", consoleWrite)
         return () => {
-            /**
-             * Will unmount section start
-             */
+            document.removeEventListener("click", consoleWrite)
+            consoleFin()
+        }
+    }, [])
+    React.useEffect(() => {
+        const consoleWrite = () => {
+            console.log(
+                "useEffect с прослушкой изменений в трех элементах [countMin, countSec, countMs] для вызова перерендера всего компонента в связи с происходящими изменениями"
+            )
+        }
+
+        let handleMinutes = () => {
+            if (+countMin < 59) {
+                setCountMin(`${+countMin + 1}`.padStart(2, countMin))
+            } else {
+                setCountMin("00")
+            }
+        }
+        let handleSeconds = () => {
+            if (+countSec < 59) {
+                setCountSec(`${+countSec + 1}`.padStart(2, countSec))
+            } else {
+                setCountSec("00")
+                handleMinutes()
+            }
+        }
+        let handleMSeconds = () => {
+            if (+countMs < 99) {
+                setCountMs(`${+countMs + 1}`.padStart(2, countMs))
+            } else {
+                setCountMs("00")
+                handleSeconds()
+            }
+        }
+        let interval = setInterval(() => handleMSeconds(), 10)
+        document.addEventListener("click", consoleWrite)
+        return () => {
             clearInterval(interval)
-            /**
-             * Will unmount section end
-             */
+            document.removeEventListener("click", consoleWrite)
         }
     }, [countMin, countSec, countMs])
-
-    let handleMinutes = () => {
-        if (+countMin < 59) {
-            setCountMin(`${+countMin + 1}`.padStart(2, countMin))
-        } else {
-            setCountMin("00")
-        }
-    }
-    let handleSeconds = () => {
-        if (+countSec < 59) {
-            setCountSec(`${+countSec + 1}`.padStart(2, countSec))
-        } else {
-            setCountSec("00")
-            handleMinutes()
-        }
-    }
-    let handleMSeconds = () => {
-        if (+countMs < 99) {
-            setCountMs(`${+countMs + 1}`.padStart(2, countMs))
-        } else {
-            setCountMs("00")
-            handleSeconds()
-        }
-    }
 
     return (
         <div id="timer_container">
